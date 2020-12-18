24908df51cb437f1e3a3b96d3ae044fb54be63c2
Johnny Gill <swfiua@gmail.com>
2020-12-18 14:38:40 -0500

    four and a pause all sorts of horrors

--- four.py
+++ four.py
@@ -16,61 +16,76 @@ def check(data, required=None):
 
     return True
 
-def bad_year(year, start, end):
+def bad_year(key, year, start, end):
 
     year = int(year)
     if year < start or year > end:
+        print(key, year)
         return True
     return False
 
-def bad_height(value):
+def bad_height(key, value):
 
     units = value[-2:]
     #print(units)
 
-    if units not in ['cm', 'in']: return True
+    if units not in ['cm', 'in']:
+        print(key, value)
+        return True
+
     height = int(value[:-2])
 
     if units == 'in':
-        if height < 59 or height > 76: return True
-    if height < 150 or height > 193: return True
+        if height < 59 or height > 76:
+            print(key, value)
+            return True
+
+    elif height < 150 or height > 193:
+        print(key, value)
+        return True
 
     return False
 
-def bad_id(pid):
-    return len(pid) != 9
+def bad_id(key, pid):
 
-def bad_hair(value):
+    result = len(pid) != 9
 
-    if len(value) != 7: return True
+    if result:
+        print(key, pid)
 
-    if value[0] != '#': return True
+    return result
+    
+
+def bad_hair(key, value):
+
+    result = False
+    if len(value) != 7: result = True
+
+    if value[0] != '#': result = True
 
     for c in value[1:]:
-        if c not in '0123456789abcdef': return True
+        if c not in '0123456789abcdef': result = True
 
-    return False
+    if result:
+        print(key, value)
+        
+    return result
         
 
 def check2(data):
 
     eyes = set('amb blu brn gry grn hzl oth'.split())
     for key, value in data.items():
-        if key == 'byr':
-            if bad_year(value, 1920, 2020):
-                return False
-        if key == 'hcl' and bad_hair(value): return False
-        if key == 'iyr' and bad_year(value, 2010, 2020): return False
-        if key == 'eyr' and bad_year(value, 2020, 2030): return False
-        if key == 'hgt' and bad_height(value): return False
+        if key == 'byr' and bad_year(key, value, 1920, 2002): return False
+        if key == 'iyr' and bad_year(key, value, 2010, 2020): return False
+        if key == 'eyr' and bad_year(key, value, 2020, 2030): return False
+        if key == 'hcl' and bad_hair(key, value): return False
+        if key == 'hgt' and bad_height(key, value): return False
         if key == 'ecl' and value not in eyes: return False
-        if key == 'pid' and bad_id(value): return False
+        if key == 'pid' and bad_id(key, value): return False
 
     return True
             
-
-        
-
 def get_fields(data):
 
     result = {}
@@ -98,7 +113,7 @@ def parse(passports):
                     if check2(fields):
                         magic += 1
                     else:
-                        print(passport)
+                        print(len(fields))
             passport = []
         else:
             passport.append(row)
