4338818b62fbc53216a375401d1f9d17497c8c92
Johnny Gill <swfiua@gmail.com>
2020-12-17 19:00:53 -0500

    three phase two first stab

--- three.py
+++ three.py
@@ -4,22 +4,43 @@ maze = open('three.csv').read().split('\n')
 
 rows, cols = len(maze), len(maze[0])
 
-row, col = 0, 0
 
-hits = 0
+def tree_hits(maze, right=3, down=1):
 
-while row < rows:
+    row, col = 0, 0
 
-    # ignore blank lines
-    if maze[row]:
+    hits = 0
+
+    while row < rows:
+
+        # ignore blank lines
+        if maze[row]:
+        
+            if maze[row][col] == '#':
+                hits += 1
+
+        row += down
+        col += right
+        if col >= cols:
+            col -= cols
+
+    return hits
+
+
+data = (
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2))
+
+result = 1
+
+for (right, down) in data:
+    hits = tree_hits(maze, right, down)
+
+    print(hits)
     
-        if maze[row][col] == '#':
-            hits += 1
+    result *= hits
 
-    row += 1
-    col += 3
-    if col >= cols:
-        col -= cols
-
-print(hits)
-    
+print(result)
